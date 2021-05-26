5dc67ed3a6859db8770b53b09e6fae6722b23472
MarielaKaradzhova <marielakaradzhova@gmail.com>
2021-05-26 03:11:43 -0400

    moving files

---
+++ schema.sql
@@ -0,0 +1,75 @@
+DROP TABLE departments CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE salaries CASCADE;
+DROP TABLE titles CASCADE;
+DROP TABLE dept_emp CASCADE;
+
+-- Creating tables for PH-EmployeeDB
+
+-- Create Departments Table
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+-- Create Employee Table
+CREATE TABLE employees ( 
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender CHAR NOT NULL, --CHAR
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+
+-- Create Department Manager Table
+CREATE TABLE dept_manager (
+   dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create Salaries Table
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+--Create Titles Table
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title  VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+--Create Table Department Employees
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+SELECT*FROM departments;
+SELECT*FROM employees;
+SELECT*FROM dept_manager;
+SELECT*FROM dept_emp; -- not working
+SELECT*FROM salaries;
+SELECT*FROM titles; -- not working
+
