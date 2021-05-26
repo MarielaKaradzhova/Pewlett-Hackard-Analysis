bd8312b0a400831b3630ae6d8c6b5c9de61ee455
MarielaKaradzhova <marielakaradzhova@gmail.com>
2021-05-26 02:29:23 -0400

    adding changes to query file and tables

--- Data/queries.sql
+++ Data/queries.sql
@@ -69,9 +69,9 @@ CREATE TABLE dept_emp (
 SELECT*FROM departments;
 SELECT*FROM employees;
 SELECT*FROM dept_manager;
-SELECT*FROM dept_emp; -- not working
+SELECT*FROM dept_emp; -- 
 SELECT*FROM salaries;
-SELECT*FROM titles; -- not working
+SELECT*FROM titles; -- 
 
 
 --Analysis
@@ -128,3 +128,69 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT*FROM retirement_info;
+
+-- Joins
+--Recreate the retirement_info Table with the emp_no Column
+-- Create new table for retiring employees
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+
+#Use Left Join to Capture retirement-info Table
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+#Use Aliases for Code Readability retirement info join
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+#Use Aliases for Code Readability departments join
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+     FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+#Use Left Join for retirement_info and dept_emp tables
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
